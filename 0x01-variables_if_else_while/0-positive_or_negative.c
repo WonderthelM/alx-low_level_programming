995c070b0c422a5fa1a6bb63f843ad54d6880e25
WonderthelM <poppymatamela@gmail.com>
2023-10-09 00:57:28 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-positive_or_negative.c -o 0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main - Entry prints if number is positive, negative or zero
+ * Return: Always (Success)
  */
 
 int main(void)
@@ -12,12 +12,19 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX /2;
+	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
+	{
 		printf("%d is positive\n", n);
+	}
 	else if (n == 0)
+	{
 		printf("%d is zero\n", n);
+	}
 	else
+	{
 		printf("%d is negative\n", n);
+	}
 	return (0);
 }
